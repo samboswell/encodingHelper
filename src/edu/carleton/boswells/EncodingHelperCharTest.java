9319f94d4bfd3c3b2112edc9e9a86ddb5ea922bd
Bosbetter <boswells@carleton.edu>
2015-04-05 17:46:35 -0500

    skeleton of encoding helper test

---
+++ src/edu/carleton/boswells/EncodingHelperCharTest.java
@@ -0,0 +1,41 @@
+package edu.carleton.boswells;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by boswells on 4/5/15.
+ */
+public class EncodingHelperCharTest {
+
+    @Test
+    public void testGetCodePoint() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCodePoint() throws Exception {
+
+    }
+
+    @Test
+    public void testToUtf8Bytes() throws Exception {
+
+    }
+
+    @Test
+    public void testToCodePointString() throws Exception {
+
+    }
+
+    @Test
+    public void testToUtf8String() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCharacterName() throws Exception {
+
+    }
+}
