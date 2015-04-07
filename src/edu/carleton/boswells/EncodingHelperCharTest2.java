fac0924df1860bc56e58b96354c31ed0d7ee35fd
Bosbetter <boswells@carleton.edu>
2015-04-07 18:24:20 -0500

    CharacterName added

---
+++ src/edu/carleton/boswells/EncodingHelperCharTest2.java
@@ -0,0 +1,41 @@
+package edu.carleton.boswells;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by boswells on 4/7/15.
+ */
+public class EncodingHelperCharTest2 {
+
+    @Test
+    public void testGetCodePoint() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCodePoint() throws Exception {
+
+    }
+
+    @Test
+    public void testToUtf8Bytes() throws Exception {
+
+    }
+
+    @Test
+    public void testToCodePointString() throws Exception {
+
+    }
+
+    @Test
+    public void testToUtf8String() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCharacterName() throws Exception {
+
+    }
+}
