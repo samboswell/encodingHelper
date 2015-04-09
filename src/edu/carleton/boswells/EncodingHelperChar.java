5d19d94ff4c1e156bf0dabf753e73124eef93f41
Michael Stoneman <azureillusions@eduroam-171-201.dyn.carleton.edu>
2015-04-08 21:20:10 -0500

    I really hate the lab computer right now. Thank god for pastebin.

--- src/edu/carleton/boswells/EncodingHelperChar.java
+++ src/edu/carleton/boswells/EncodingHelperChar.java
@@ -6,16 +6,21 @@ import java.io.*;
  * The main model class for the EncodingHelper project in
  * CS 257, Spring 2015, Carleton College. Each object of this class
  * represents a single Unicode character. The class's methods
- * generate various representations of the character. 
+ * generate various representations of the character.
  */
 public class EncodingHelperChar {
     private int codePoint;
     private String hexString;
 
     public EncodingHelperChar(int codePoint) {
-        this.codePoint = codePoint;
+        if (0 < codePoint && codePoint < (0x10FFFF)) {
+            this.codePoint = codePoint;
+        }
+        else {
+            throw new IllegalArgumentException("Code point is out of bounds (either too high or too low)");
+        }
     }
-    
+
     public EncodingHelperChar(byte[] utf8Bytes) {
         if (utf8Bytes.length == 1) {
             this.codePoint = utf8Bytes[0];
@@ -28,62 +33,60 @@ public class EncodingHelperChar {
         } else if (utf8Bytes.length == 4) {
             this.codePoint = ((utf8Bytes[0] & 0x7) << 18) + ((utf8Bytes[1] & 0x3F) << 12) + ((utf8Bytes[2] & 0x3F) << 6) + (utf8Bytes[3] & 0x3F);
             //Bytes have format 11110xxx 10xxxxxx 10xxxxxx 10xxxxx
-        } else if (utf8Bytes.length == 5) {
-            this.codePoint = ((utf8Bytes[0] & 0x3) << 24) + ((utf8Bytes[1] & 0x3F) << 18) + ((utf8Bytes[2] & 0x3F) << 12) + ((utf8Bytes[3] & 0x3F) << 6) + (utf8Bytes[4] & 0x3F);
-            //Bytes have format 111110xx 10xxxxxx 10xxxxxx 10xxxxx 10xxxxxx
-            //Gather the xes and save. I don't think we need to make it hex?
-        } else if (utf8Bytes.length == 6) {
-            this.codePoint = ((utf8Bytes[0] & 0x1) << 30) + ((utf8Bytes[1] & 0x3F) << 24) + ((utf8Bytes[2] & 0x3F) << 18) + ((utf8Bytes[3] & 0x3F) << 12) + ((utf8Bytes[4] & 0x3F) << 6) + (utf8Bytes[5] & 0x3F);
         } else {
-            this.codePoint = 0x03C0 //I wanted an error case and I like pi.
+            this.codePoint = 0x03C0; //I wanted an error case and I like pi.
         }
     }
-    
+
+
     public EncodingHelperChar(char ch) {
-        chStr = Character.toString(ch);
-        this.codepoint = chStr.codePointAt(0);
+        this.codePoint = Character.getNumericValue(ch);
+        if (0 > codePoint || codePoint > (0x10FFFF)) {
+            throw new IllegalArgumentException("Code point is out of bounds (either too high or too low)");
+        }
+
+        // I BELIEVE that you can literally just convert a char to an int to get
+        //the code point. We'll see!
     }
-    
+
     public int getCodePoint() {
         return this.codePoint;
     }
-    
+
     public void setCodePoint(int codePoint) {
         this.codePoint = codePoint;
     }
-    
+
     /**
      * Converts this character into an array of the bytes in its UTF-8
      * representation.
      *   For example, if this character is a lower-case letter e with an acute
      * accent, whose UTF-8 form consists of the two-byte sequence C3 A9, then
      * this method returns a byte[] of length 2 containing C3 and A9.
-     * 
+     *
      * @return the UTF-8 byte array for this character
      */
     public byte[] toUtf8Bytes() {
+        byte[] arrayOfBytes = null;
         if (codePoint < 0x80) { //if codepoint < 128 { [0xxxxxxx] }
-            byte[] arrayOfBytes = new byte[1];
+            arrayOfBytes = new byte[1];
             arrayOfBytes[0] = (byte) codePoint;
-            return arrayOfBytes;
         } else if (codePoint < 0x800) { //[110xxxxx, 10xxxxxx]
-            byte[] arrayOfBytes = new byte[2];
+            arrayOfBytes = new byte[2];
             int firstInt = ((codePoint & 0x7C0) >> 6) + 0xC0;
             arrayOfBytes[0] = (byte) firstInt;
             int secondInt = (codePoint & 0x3F) + 0x80;
             arrayOfBytes[1] = (byte) secondInt;
-            return arrayOfBytes;
         } else if (codePoint < 0x10000) { //[1110xxxx, 10xxxxxx, 10xxxxxx]
-            byte[] arrayOfBytes = new byte[3];
+            arrayOfBytes = new byte[3];
             int firstInt = ((codePoint & 0xF000) >> 12) + 0xE0;
             arrayOfBytes[0] = (byte) firstInt;
             int secondInt = ((codePoint & 0xFC0) >> 6) + 0x80;
             arrayOfBytes[1] = (byte) secondInt;
             int thirdInt = (codePoint & 0x3F) + 0x80;
             arrayOfBytes[2] = (byte) thirdInt;
-            return arrayOfBytes;
         } else if (codePoint < 0x200000) { //[11110xxx, 10xxxxxx, 10xxxxxx, 10xxxxxx]
-            byte[] arrayOfBytes = new byte[4];
+            arrayOfBytes = new byte[4];
             int firstInt = ((codePoint & 0x1C0000) >> 18) + 0xF0;
             arrayOfBytes[0] = (byte) firstInt;
             int secondInt = ((codePoint & 0x3F000) >> 12) + 0x80;
@@ -92,38 +95,10 @@ public class EncodingHelperChar {
             arrayOfBytes[2] = (byte) thirdInt;
             int fourthInt = (codePoint & 0x3F) + 0x80;
             arrayOfBytes[3] = (byte) fourthInt;
-            return arrayOfBytes;
-        } else if (codePoint < 0x4000000) { //[111110xx, 10xxxxxx, 10xxxxxx, 10xxxxxx, 10xxxxxx]
-            byte[] arrayOfBytes = new byte[5];
-            int firstInt = ((codePoint & 0x3000000) >> 24) + 0xF8;
-            arrayOfBytes[0] = (byte) firstInt;
-            int secondInt = ((codePoint & 0xFC0000) >> 18) + 0x80;
-            arrayOfBytes[1] = (byte) secondInt;
-            int thirdInt = ((codePoint & 0x3F000) >> 12) + 0x80;
-            arrayOfBytes[2] = (byte) thirdInt;
-            int fourthInt = ((codePoint & 0xFC0) >> 6) + 0x80;
-            arrayOfBytes[3] = (byte) fourthInt;
-            int fifthInt = (codePoint & 0x3F) + 0x80;
-            arrayOfBytes[4] = (byte) fifthInt;
-            return arrayOfBytes;
-        } else { //[1111110x, 10xxxxxx, 10xxxxxx, 10xxxxxx, 10xxxxxx, 10xxxxxx]
-            byte[] arrayOfBytes = new byte[6];
-            int firstInt = ((codePoint & 0x40000000) >> 30) + 0xFC;
-            arrayOfBytes[0] = (byte) firstInt;
-            int secondInt = ((codePoint & 0x3F000000) >> 24) + 0x80;
-            arrayOfBytes[1] = (byte) secondInt;
-            int thirdInt = ((codePoint & 0xFC0000) >> 18) + 0x80;
-            arrayOfBytes[2] = (byte) thirdInt;
-            int fourthInt = ((codePoint & 0x3F000) >> 12) + 0x80;
-            arrayOfBytes[3] = (byte) fourthInt;
-            int fifthInt = ((codePoint & 0xFC0) >> 6) + 0x80;
-            arrayOfBytes[4] = (byte) fifthInt;
-            int sixthInt = (codePoint & 0x3F) + 0x80;
-            arrayOfBytes[5] = (byte) sixthInt;
-            return arrayOfBytes;
         }
+        return arrayOfBytes;
     }
-    
+
     /**
      * Generates the conventional 4-digit hexadecimal code point notation for
      * this character.
@@ -134,13 +109,13 @@ public class EncodingHelperChar {
      * @return the U+ string for this character
      */
     public String toCodePointString() {
-        hexString = Integer.toHexString(codePoint);
+        hexString = String.format("U+%04X", codePoint);
         // There's a method for Integer called toHexString(). It takes an
         // Integer object and prints the hex string for it. We can probably use
         // that, then just add U+ to the front.
-        return "U+" + hexString;
+        return hexString;
     }
-    
+
     /**
      * Generates a hexadecimal representation of this character suitable for
      * pasting into a string literal in languages that support hexadecimal byte
@@ -152,12 +127,18 @@ public class EncodingHelperChar {
      * @return the escaped hexadecimal byte string
      */
     public String toUtf8String() {
+        byte[] encodingArray = toUtf8Bytes();
+        String encodingString = "";
+        for (int i = 0; i < encodingArray.length; i++) {
+            String encodedByte = String.format("\\x%02X", encodingArray[i]);
+            encodingString = encodingString.concat(encodedByte);
+        }
         // This one's iffier for me. We could use toUtf8Bytes, then somehow turn
         // the byte into an int, then use the toHexString() method, then parse
         // it to add the slashes and xes and such?
-        return "";
+        return encodingString;
     }
-    
+
     /**
      * Generates the official Unicode name for this character.
      *   For example, if this character is a lower-case letter e with an acute
@@ -174,25 +155,28 @@ public class EncodingHelperChar {
         // parse that out.) The document goes <codepoint>;<name>;(other crap),
         // so we want to split the string on semicolons and grab the second
         // thing. Then return it.
-        hexString = Integer.toHexString(codePoint);
-        Scanner unicodeData = null;
+        hexString = String.format("%04X", codePoint);
+        String charName = null;
+        //String finalCharName = null;
+        String[] charNameArray = null;
+
         try{
-            unicodeData = new Scanner(new FileReader("UnicodeData.txt"));
+            Scanner unicodeData = new Scanner(new FileReader("src/edu/carleton/boswells/UnicodeData.txt"));
+            unicodeData.useDelimiter(";");
+            while (unicodeData.hasNextLine()) {
+                charName = unicodeData.findInLine(hexString);
+                if (charName != null) {
+                    break;
+                }
+                unicodeData.nextLine();
+            }
+            if (charName == null) { return "Code point is unassigned. No character name generated";
+            }
         }
         catch (FileNotFoundException e){
             System.err.println("File name not found");
         }
-        unicodeData.useDelimiter(";");
-        String charName = null;
-        while (unicodeData.hasNextLine()) {
-            charName = unicodeData.findInLine(hexString);
-            if (charName != null) {
-                break; }
-            unicodeData.nextLine();
-        }
-        if (charName == null) { return "Code point is invalid. No character name generated";
-        }
-        String[] charNameArray = charName.split(";");
-        return charNameArray[1];
+
+        return charName;
     }
 }
--- src/edu/carleton/boswells/EncodingHelperCharTest.java
+++ src/edu/carleton/boswells/EncodingHelperCharTest.java
@@ -1,65 +1,149 @@
 package edu.carleton.boswells;
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 /**
  * Created by Sam Boswell and Michael Stoneman, last edited on 4/5/15.
  */
 public class EncodingHelperCharTest {
+    /**
+     @Test
+     public void EncodingHelperChar(char ch) throws Exception {
+     EncodingHelperChar T1 = new EncodingHelperChar('?');
+
+     }
+
+     @Test
+     public void EncodingHelperCharShouldReturnInvalid(char ch) throws Exception {
+
+
+     }
+
+     @Test
+     public void EncodingHelperChar(int codePoint) throws Exception {
+     EncodingHelperChar T1 = new EncodingHelperChar(0xA5);
+     assertEquals(T1.getCodePoint(), 0xA5);
+     }
+
+     @Test
+     public void EncodingHelperCharInvalidInt(int codePoint) throws Exception {
+     EncodingHelperChar T2 = new EncodingHelperChar(0x10FFFF);
+     assertEquals("Invalid int - too high","IllegalArgumentException - int is too large", "IllegalArgumentException - int is too large");
+     }
+
+     @Test
+     public void EncodingHelperCharNegativeInt(int codePoint) throws Exception {
+     EncodingHelperChar T3 = new EncodingHelperChar(-0x80);
+     assertEquals("Invalid int - too high","IllegalArgumentException - int is too large", "IllegalArgumentException - int is too large");
+     }
+
+     @Test
+     public void EncodingHelperCharInvalidByteTypeShouldReturnInvalid(byte[] utf8Bytes) throws Exception {
+     EncodingHelperChar T4 = new EncodingHelperChar(0x10FFFF);
+
+     }
+
+     @Test
+     public void EncodingHelperChar(byte[] utf8Bytes) throws Exception {
+     EncodingHelperChar T5 = new EncodingHelperChar(0xA5);
+
+     }
+     */
 
     @Test
     /**
      * Tests getting code points for EncodingHelperChar.
-     * @exception if it gets the wrong code point.
+     * @exception it gets the wrong code point.
      */
     public void testGetCodePoint() throws Exception {
-        EncodingHelperChar test1 = new EncodingHelperChar(0xA4);
-        assertEquals(test1.getCodePoint(), 0xA4);
+        EncodingHelperChar test1 = new EncodingHelperChar(0xA5);
+        assertEquals(test1.getCodePoint(), 0xA5);
     }
 
     @Test
     /**
      * Tests setting code points for EncodingHelperChar.
-     * @exception if the code point is set to a new code point, but
+     * @exception the code point is set to a new code point, but
      * returns the original code point.
      */
     public void testSetCodePoint() throws Exception {
-        EncodingHelperChar test2 = new EncodingHelperChar(0xA4);
-        test2.setCodePoint(0xA5);
-        assertEquals(test2.getCodePoint(), 0xA5);
+        EncodingHelperChar test2 = new EncodingHelperChar(0xA5);
+        test2.setCodePoint(0xA6);
+        assertEquals(test2.getCodePoint(), 0xA6);
     }
 
     @Test
     /**
      * Tests converting the code point to UTF-8 bytes for EncodingHelperChar.
-     * @exception if the code point does not convert correctly to UTF-8 bytes.
+     * @exception the code point does not convert correctly to UTF-8 bytes.
      */
     public void testToUtf8Bytes() throws Exception {
-        EncodingHelperChar test3 = new EncodingHelperChar(0xA4);
-        String codePoint = "C2 A4";
-        byte[] utf8Bytes = codePoint.getBytes();
-        assertEquals("Failure - incorrect UTF-8 bytes",test3.toUtf8Bytes(),utf8Bytes);
+        EncodingHelperChar test3 = new EncodingHelperChar(0xA5);
+        byte[] utf8Bytes = new byte[]{(byte) 194, (byte) 164};
+        assertArrayEquals("Failure - incorrect UTF-8 bytes", utf8Bytes, test3.toUtf8Bytes());
     }
-
+    /** @Test
+    public void testToUtf8BytesLength2() throws Exception {
+    EncodingHelperChar test3 = new EncodingHelperChar(0x80);
+    byte[] utf8Bytes = [11000010 10100100];
+    assertArrayEquals("Failure - incorrect UTF-8 bytes", utf8Bytes, test3.toUtf8Bytes());
+    }
+     @Test
+     public void testToUtf8BytesLength3() throws Exception {
+     EncodingHelperChar test3 = new EncodingHelperChar(0x800);
+     byte[] utf8Bytes = [11000010 10100100];
+     assertArrayEquals("Failure - incorrect UTF-8 bytes", utf8Bytes, test3.toUtf8Bytes());
+     }
+     @Test
+     public void testToUtf8BytesLength4() throws Exception {
+     EncodingHelperChar test3 = new EncodingHelperChar(0x10000);
+     byte[] utf8Bytes = [11000010 10100100];
+     assertArrayEquals("Failure - incorrect UTF-8 bytes", utf8Bytes, test3.toUtf8Bytes());
+     }
+     @Test
+     public void testToUtf8BytesLength5() throws Exception {
+     EncodingHelperChar test3 = new EncodingHelperChar(0x200000);
+     byte[] utf8Bytes = [11000010 10100100];
+     assertArrayEquals("Failure - incorrect UTF-8 bytes", utf8Bytes, test3.toUtf8Bytes());
+     }
+     @Test
+     public void testToUtf8BytesLength6() throws Exception {
+     EncodingHelperChar test3 = new EncodingHelperChar(0x4000000);
+     byte[] utf8Bytes = [11000010 10100100];
+     assertArrayEquals("Failure - incorrect UTF-8 bytes", utf8Bytes, test3.toUtf8Bytes());
+     }
+     @Test
+     public void testToUtf8BytesOutsideOfBounds() throws Exception {
+     EncodingHelperChar test3 = new EncodingHelperChar(0x11000000);
+     byte[] utf8Bytes = [11000010 10100100];
+     assertArrayEquals("Failure - incorrect UTF-8 bytes", utf8Bytes, test3.toUtf8Bytes());
+     }
+     */
     @Test
     /**
      * Tests outputting the code point in a string format for EncodingHelperChar.
-     * @exception if the correct code point is not output as a string.
+     * @exception the correct code point is not output as a string.
      */
 
     public void testToCodePointString() throws Exception {
-        EncodingHelperChar test4 = new EncodingHelperChar(0xA4);
-        assertEquals("Failure - incorrect code point string", test4.toCodePointString(), "U+00A4");
+        EncodingHelperChar test4 = new EncodingHelperChar(0xA5);
+        assertEquals("Failure - incorrect code point string", "U+00A5", test4.toCodePointString());
     }
 
     @Test
     /**
      * Tests printing a string of the UTF-8 encoding of the code point.
-     * @exception if the string does not match the expected encoding string.
+     * @exception the string does not match the expected encoding string.
      */
     public void testToUtf8String() throws Exception {
-        EncodingHelperChar test5 = new EncodingHelperChar(0xA4);
-        assertEquals("Failure - incorrect Utf8 string",test5.toUtf8String(), "/xC2/xA5" );
+        EncodingHelperChar test5 = new EncodingHelperChar(0xA5);
+        assertEquals("Failure - incorrect Utf8 string", "\\xC2\\xA5", test5.toUtf8String() );
+    }
+    @Test
+    public void testToUtf8String3() throws Exception {
+        EncodingHelperChar test5 = new EncodingHelperChar(0xA5);
+        assertEquals("Failure - incorrect Utf8 string", "\\xC2\\xA5", test5.toUtf8String() );
     }
 
     @Test
@@ -69,7 +153,25 @@ public class EncodingHelperCharTest {
      * character name.
      */
     public void testGetCharacterName() throws Exception {
-        EncodingHelperChar test6 = new EncodingHelperChar(0xA4);
-        assertEquals("Failure - incorrect character name", test6.getCharacterName(), "YEN SIGN");
+        EncodingHelperChar test6 = new EncodingHelperChar(0xA5);
+        assertEquals("Failure - incorrect character name", "YEN SIGN", test6.getCharacterName());
+    }
+
+    @Test
+    public void testGetCharacterNameForControlCharacters() throws Exception {
+        EncodingHelperChar test6 = new EncodingHelperChar(0x1A);
+        assertEquals("Failure - incorrect character name", "<control> SUBSTITUTE", test6.getCharacterName());
+    }
+
+    @Test
+    public void testGetCharacterNameForHigherRangeControlCharacters() throws Exception {
+        EncodingHelperChar test6 = new EncodingHelperChar(0x9F);
+        assertEquals("Failure - incorrect character name", "<control> APPLICATION CONTROL COMMAND", test6.getCharacterName());
+    }
+
+    @Test
+    public void testGetCharacterNameForUnknownCharacters() throws Exception {
+        EncodingHelperChar test6 = new EncodingHelperChar(0x3FFF);
+        assertEquals("Failure - incorrect character name", "<unknown> U+3FFF", test6.getCharacterName());
     }
 }
