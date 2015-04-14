7d467f21e577817569b5c1cd5c199a3fe54ed6f0
Bosbetter <boswells@carleton.edu>
2015-04-13 20:42:17 -0500

    Actually added EncodingHelperHelperhelperhelper.

---
+++ src/EncodingHelper.java
@@ -0,0 +1,8 @@
+/**
+ * Created by boswells on 4/12/15.
+ */
+public class EncodingHelper {
+    public static void main(String[] args) {
+        edu.carleton.boswells.EncodingHelper.main(args);
+    }
+}
